4f894bb7f3f239497de562c99d2b459d945ebac3
Schmenns <54784861+Schmenns@users.noreply.github.com>
2020-12-23 08:12:39 -0500

    Ask function and other things

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 const { BrowserWindowProxy } = require('electron');
 const electron = require('electron');
 
+const addClass = document.querySelector('#class-add');
+
 class Class {
   constructor(subject, url) {
     this.subject = subject;
@@ -11,8 +13,33 @@ class Class {
 }
 
 let classes = [
-  { label: 'English', url: 'https://meet.google.com' },
+  { label: 'English', url: 'https://meet.google.com/hsq-jqob-nmu' },
 ];
+
+const ask = question => {
+  const container = Object.assign(document.createElement('div'), { id: 'ask-container' });
+  const subcontainer = Object.assign(document.createElement('div'), { id: 'ask-subcontainer' });
+  const background = Object.assign(document.createElement('div'), { id: 'ask-background' });
+
+  const text = Object.assign(document.createElement('h3'), { id: 'ask-text', innerText: question });
+  const input = Object.assign(document.createElement('input'), { id: 'ask-input' });
+
+  const imageInput = Object.assign(document.createElement('input'), { id: 'ask-image-input', type: 'file' });
+  const imageInputLabel = Object.assign(document.createElement('label'), { id: 'ask-image-input-label', for: 'ask-image-input' });
+  imageInputLabel.onclick = () => imageInput.click();
+
+  const submit = Object.assign(document.createElement('button'), { id: 'ask-submit', innerText: 'Submit' })
+
+  subcontainer.append(text, input, document.createElement('br'), imageInputLabel, document.createElement('br'), submit);
+  container.append(background, subcontainer);
+
+  document.body.append(container);
+  
+  submit.onclick = () => {
+    console.log('eodgijkm')
+    container.remove();
+  }
+}
 
 window.onload = () => {
   const container = document.querySelector('#class-container');
@@ -33,3 +60,11 @@ window.onload = () => {
     subcontainer.onclick = () => electron.ipcRenderer.send('load-url', c.url); // Runs the code to change the url in index.js                                                                  
   });
 };
+
+
+addClass.onclick = () => {
+  ask('What is the class called?')
+  document.querySelector('#ask-submit').onclick = () => {
+    document.querySelector('#ask-container').remove;
+  }
+}
